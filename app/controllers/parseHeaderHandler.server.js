8588b33bf48d0e7ba8067acb6cd03a314831f583
dvicente04 <dvicente04@alumni.depaul.edu>
2016-04-03 21:19:06 -0500

    delivery pass

--- app/controllers/parseHeaderHandler.server.js
+++ app/controllers/parseHeaderHandler.server.js
@@ -1,6 +1,10 @@
 function parseHeaderHandler(req){
-	//console.log(req);
-	return req.headers;
+	var headers = req.headers
+	return {
+		ip: headers['x-forwarded-for'] || req.connection.remoteAddress,
+		language: headers['accept-language'].split(',')[0],
+		os: headers['user-agent'].split('(')[1].split(')')[0]
+	};
 }
 
 module.exports = parseHeaderHandler;
